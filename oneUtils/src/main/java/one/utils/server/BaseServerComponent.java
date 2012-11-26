cc2545890ae22c71a87e7a4cf951e7cdc40e6186
mxro <mxro@nowhere.com>
2012-11-27 12:51:16 +1300

    adding default implementation

---
+++ oneUtils/src/main/java/one/utils/server/BaseServerComponent.java
@@ -0,0 +1,25 @@
+package one.utils.server;
+
+public class BaseServerComponent implements ServerComponent {
+
+	@Override
+	public void stop(final ShutdownCallback callback) {
+		throw new RuntimeException("Method not supported");
+	}
+
+	@Override
+	public void start(final StartCallback callback) {
+		throw new RuntimeException("Method not supported");
+	}
+
+	@Override
+	public void injectConfiguration(final ComponentConfiguration<?> conf) {
+		throw new RuntimeException("Method not supported");
+	}
+
+	@Override
+	public void injectContext(final ComponentContext context) {
+		throw new RuntimeException("Method not supported");
+	}
+
+}
