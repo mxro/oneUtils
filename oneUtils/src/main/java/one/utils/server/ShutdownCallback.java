77c5a3c0ba3e7aee82b1f464940f9122d7141374
mxro <mxro@nowhere.com>
2012-11-30 10:03:00 +1300

    updating javadoc

--- oneUtils/src/main/java/one/utils/server/ShutdownCallback.java
+++ oneUtils/src/main/java/one/utils/server/ShutdownCallback.java
@@ -5,8 +5,23 @@
  ******************************************************************************/
 package one.utils.server;
 
+/**
+ * Callback, which can be used, when a service provider is shut down.
+ * 
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ * 
+ */
 public interface ShutdownCallback {
+
+	/**
+	 * This method is called when the shutdown could be completed successfully.
+	 */
 	public void onShutdownComplete();
 
+	/**
+	 * This method is called when an error occurs in the shutdown process.
+	 * 
+	 * @param t
+	 */
 	public void onFailure(Throwable t);
 }
