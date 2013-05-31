486d007ad4fb4a5ef84142920def417263f9a03d
mxro <mxro@nowhere.com>
2013-05-31 14:42:52 +1200

    adding utility for counting character occurences in a string

--- oneUtils/src/main/java/one/utils/OneUtilsStrings.java
+++ oneUtils/src/main/java/one/utils/OneUtilsStrings.java
@@ -28,4 +28,30 @@ public class OneUtilsStrings {
         }
         return found;
     }
+
+    /**
+     * Counts the occurrences of <code>character</code> in <code>inStr</code>.
+     * Starts scanning the <code>inStr</code> from the <b>end</b>. The first
+     * time <code>untilChar</code> is encountered, the counting will immediately
+     * stop.
+     * 
+     * @param character
+     * @param inStr
+     * @param untilChar
+     * @return
+     */
+    public static int characterOccurs(final char character, final String inStr,
+            final char untilChar) {
+        int counter = 0;
+        for (int i = inStr.length() - 1; i >= 0; i--) {
+
+            final char comparChar = inStr.charAt(i);
+            if (comparChar == character) {
+                counter++;
+            } else if (comparChar == untilChar) {
+                return counter;
+            }
+        }
+        return counter;
+    }
 }
