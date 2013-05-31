15265c69fb3bbac812a3d59a7327e6db9fee70c6
mxro <mxro@nowhere.com>
2013-05-31 14:29:39 +1200

    removing '.' from list of allowed characters - '.' created complications on the Appjangle cloud Rest server, where a '.' in an object name would prevent the REST server from determining the variant of a node correctly.

--- oneUtils/src/main/java/one/utils/OneUtilsStrings.java
+++ oneUtils/src/main/java/one/utils/OneUtilsStrings.java
@@ -8,24 +8,24 @@ package one.utils;
  */
 public class OneUtilsStrings {
 
-	public static final char[] allowedCharacters = { 'a', 'b', 'c', 'd', 'e',
-			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-			's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-', '.', '1', '2',
-			'3', '4', '5', '6', '7', '8', '9', '0' };
+    public static final char[] allowedCharacters = { 'a', 'b', 'c', 'd', 'e',
+            'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '0' };
 
-	/**
-	 * Returns true if the given character is a 'standard' character. (a-z,
-	 * A-Z).
-	 * 
-	 * @param character
-	 * @return
-	 */
-	public static boolean isSimpleCharacter(final char character) {
-		boolean found = false;
-		for (final char element : allowedCharacters) {
-			found = found || character == element
-					|| character == Character.toUpperCase(element);
-		}
-		return found;
-	}
+    /**
+     * Returns true if the given character is a 'standard' character. (a-z,
+     * A-Z).
+     * 
+     * @param character
+     * @return
+     */
+    public static boolean isSimpleCharacter(final char character) {
+        boolean found = false;
+        for (final char element : allowedCharacters) {
+            found = found || character == element
+                    || character == Character.toUpperCase(element);
+        }
+        return found;
+    }
 }
