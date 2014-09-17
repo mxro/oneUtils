8f0ae143feb750ac1cc091fb453a921fb47b8994
mxro <mxro@nowhere.com>
2014-09-18 09:14:32 +1000

    Reworking embedded server API Autocommit Thu Sep 18 09:14:32 EST 2014

--- src/main/java/one/utils/jre/OneUtilsJre.java
+++ src/main/java/one/utils/jre/OneUtilsJre.java
@@ -3,20 +3,20 @@ package one.utils.jre;
 import java.io.IOException;
 import java.io.InputStream;
 
-import de.mxro.concurrency.Concurrency;
 import one.utils.jre.concurrent.JreConcurrency;
 import one.utils.jre.internal.IOUtils;
+import de.mxro.concurrency.Concurrency;
 
 public class OneUtilsJre {
 
-	public static Concurrency newJreConcurrency() {
-		return new JreConcurrency();
-	}
+    // TODO move static method to ConcurrencyJre
+    public static Concurrency newJreConcurrency() {
+        return new JreConcurrency();
+    }
 
-	public final static byte[] toByteArray(final InputStream is)
-			throws IOException {
+    public final static byte[] toByteArray(final InputStream is) throws IOException {
 
-		return IOUtils.toByteArray(is);
+        return IOUtils.toByteArray(is);
 
-	}
+    }
 }
