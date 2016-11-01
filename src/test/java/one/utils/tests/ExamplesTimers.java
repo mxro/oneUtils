626df2334f23f11dd5652fedcfbf416993f44e42
mxro <mxro@nowhere.com>
2016-11-01 16:22:35 +1100

    Working on ObjectHub and Stellar DB Autocommit Tue Nov 01 16:22:35 AEDT 2016

--- src/test/java/one/utils/tests/ExamplesTimers.java
+++ src/test/java/one/utils/tests/ExamplesTimers.java
@@ -6,47 +6,37 @@ import one.utils.jre.OneUtilsJre;
 
 public class ExamplesTimers {
 
-	/**
-	 * @param args
-	 */
-	public static void main(final String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
 
-		final Concurrency con = OneUtilsJre.newJreConcurrency();
-		// ^-- replace with 'new GwtConcurrency()' for GWT environments
-		// see https://gist.github.com/2791639
+        final Concurrency con = OneUtilsJre.newJreConcurrency();
+        // ^-- replace with 'new GwtConcurrency()' for GWT environments
+        // see https://gist.github.com/2791639
 
-		// -----
-		// Timer for one invocation
-		// -----
-		con.newTimer().scheduleOnce(200, new Runnable() {
+        // -----
+        // Timer for one invocation
+        // -----
+        con.newTimer().scheduleOnce(200, new Runnable() {
 
-			@Override
-			public void run() {
-				System.out.println("Do in 200 ms");
-			}
-		});
+            @Override
+            public void run() {
+                System.out.println("Do in 200 ms");
+            }
+        });
 
-		// -----
-		// Timer for multiple invocations
-		// -----
-		con.newTimer().scheduleRepeating(200, 100, new Runnable() {
+        // -----
+        // Timer for multiple invocations
+        // -----
+        con.newTimer().scheduleRepeating(200, 100, new Runnable() {
 
-			@Override
-			public void run() {
-				System.out.println("Do in 200 ms and then every 100 ms.");
-			}
-		});
+            @Override
+            public void run() {
+                System.out.println("Do in 200 ms and then every 100 ms.");
+            }
+        });
 
-		// -----
-		// Timer with minimal delay
-		// -----
-		con.runLater(new Runnable() {
-
-			@Override
-			public void run() {
-				System.out.println("Do in background thread.");
-			}
-		});
-	}
+    }
 
 }
